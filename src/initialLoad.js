240ca3d3052bd5216dc295eb2c708ed54c559d7b
Barry Lawrence <barnoldsporrance@gmail.com>
2021-06-01 21:28:48 +0300

    landing page templated

--- src/initialLoad.js
+++ src/initialLoad.js
@@ -1,31 +1,55 @@
-const initialLoad = () =>
- console.log("This is inital load, imported to index.js, then bundled up and spat out via dist/main.js");
+const initialLoad = () =>{
+    const content = document.getElementById('content');
+ // append header
 
- const content = document.getElementById('content');
 
+ //create main container
+ const main = document.createElement('div');
+ main.setAttribute('id', 'main');
+
+//1. create background
+ const backGroundImage = document.createElement("img");
+ backGroundImage.setAttribute("src", "../src/images/salad.jpg");
+ backGroundImage.setAttribute("id", "backGroundImage");
+ main.appendChild(backGroundImage);
+
+
+ //2. create header
  const header = document.createElement('div');
+  const headerText = document.createTextNode("Cook dat Food");
+  header.setAttribute('id', 'headerID');
+  header.appendChild(headerText);
+  main.appendChild(header);
 
- const headerText = document.createTextNode("Yanni's Pizzeria");
- 
- header.setAttribute('id', 'header');
+//3. Create navBar
+const navBar = document.createElement('div');
+navBar.setAttribute('id', 'navBar');
+main.appendChild(navBar);
 
- header.appendChild(headerText);
+//4. create contact tab
+const contactTab = document.createElement('div');
+contactTab.setAttribute('id', 'contactTab');
+contactTab.setAttribute('class', 'navTab');
+contactTab.innerText = "Contact";
+navBar.appendChild(contactTab);
 
- content.appendChild(header);
+//5. create menu tab
+const menuTab = document.createElement('div');
+menuTab.setAttribute('id', 'menuTab');
+menuTab.setAttribute('class', 'navTab');
+menuTab.innerText = "Menu";
+navBar.appendChild(menuTab);
 
 
+//4. append blurb
+const blurbHolder = document.createElement('div');
+blurbHolder.setAttribute('id', 'blurbHolder');
+blurbHolder.innerText = "Welcome to Yanni's amazing food wonderland. Where dreams become reality. The pinnacle of yum. And lots of other, nice, friendly, pleasant compliments about the food. My word, it's rather good, eh!?";
+main.appendChild(blurbHolder);
+
+content.appendChild(main);
+}
 
 
-
-
-
-
-
-
-/*
-<div id='header'>Yanni's Kitchen</div>
-<image src="../src/images/pizzaBackground.jpeg"></image>
-<p id ='para'> Welcome to Yanni's amazing pizza shop. Where dreams become reality. The pinnacle of pizza. The golden pizza-cea on the hoizon. Pizza diem. The alpha, and the omega of pizza. Enjoy pizza-actualization right here.</p>
-*/
 
 export { initialLoad }
